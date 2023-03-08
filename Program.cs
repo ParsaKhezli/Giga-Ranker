2e0102499909e78af5cf0c2fac04ac47de7ec812
ParsaKhezli <127148800+ParsaKhezli@users.noreply.github.com>
2023-03-08 12:40:29 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter your mark");
+
+        int x;
+
+        x = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            Console.WriteLine("enter your mark");
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+            switch (x)
+            {
+                case < 10:
+                    Console.WriteLine("Failed");
+
+
+                    break;
+                case < 15:
+                    Console.WriteLine("Rank C");
+
+
+                    break;
+                case < 17:
+                    Console.WriteLine("Rank B");
+                    ;
+
+                    break;
+                case <= 20:
+                    Console.WriteLine("Rank A");
+
+
+                    break;
+                default:
+                    Console.WriteLine("invalid Number");
+                    break;
+            }
+
+
+
+
+            
+
+
+
+
+        }
+
+        while (true);
+
+        {
+            
+
+        }
+    }
+}
